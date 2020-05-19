7ca297e11991d949bf4280e85819f0b089f4dc2b
Moosa Zubairi <moosa.zubairi>
2020-05-19 18:15:20 -0500

    updates & fixes

--- filename-ext-fix.js
+++ filename-ext-fix.js
@@ -13,6 +13,8 @@ console.log(' Written for SF File Types');
 console.log('\\--------------------------------------------------------------/');
 
 
+const { COPYFILE_EXCL } = fs.constants;
+
 // read the input directoryPath into a string/source
 var args = process.argv.slice(2);
 if (args.length === 0 ) {
@@ -25,14 +27,29 @@ var directoryPath  = path.join('/', args[0]);
 console.log('Input direcotry: ', directoryPath);
 fs.readdir(directoryPath, (err, files) => {
   if(err) {
-    return console.error('Unable to scan direcotry: ' + err);
+    return console.error('\tError: Unable to scan direcotry: ' + err);
   }
+
+  // make an archive directory
+  var archiveDir = directoryPath + "/archive";
+  fs.mkdir(archiveDir, err => {
+   if(err) {
+     console.error("\tError: Couldn't create the archive directory ", archiveDir, err);
+     process.exit();
+   }
+  });
 
   // for each file in a directory
   files.forEach((file, i) => {
-
+    var fullyQualifiedFilePath = directoryPath + '/'+ file;
+    
+    // copy the file into the archive directory
+    let archiveFile = archiveDir + '/' + file;
+    fs.copyFile(fullyQualifiedFilePath, archiveFile, COPYFILE_EXCL, fileCopyCallback);
+    
+    // rename existing file
     (async () => {
-      let fullyQualifiedFilePath = directoryPath + '/'+ file;
+      
       let fileExt = await FileType.fromFile(fullyQualifiedFilePath);
       let newFileName = fullyQualifiedFilePath + '.' + fileExt.ext;
       fs.renameSync(fullyQualifiedFilePath, newFileName);
@@ -42,3 +59,7 @@ fs.readdir(directoryPath, (err, files) => {
     })();
   });
 })
+
+function fileCopyCallback(err, file) {
+  if (err) throw err;
+}
